66455716ab9d44f78dfabe33e67e94ad062525f7
Hatibe Kabulantok <hatibekara@gmail.com>
2021-01-26 09:49:56 -0800

    map added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-// export function hello() {
-//   console.log('Is this running?');
-// }
-console.log('Is this running?');
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaGF0aWJlIiwiYSI6ImNra2VhOTE5NjBhcGcyd281Njc5dWV4bzMifQ.hVtol98lt8seOLJ6gib24w';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
